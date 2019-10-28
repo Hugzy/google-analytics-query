038ff3cc79508cc45c2941b5bd69fe31a9f1b8d7
frederik <frede.madsen3@gmail.com>
2019-10-28 15:08:09 +0100

    initial commit

---
+++ date.py
@@ -0,0 +1,8 @@
+import datetime
+
+x = datetime.datetime(2016, 8, 1)
+
+print(x.strftime("%Y%m%d"))
+
+
+#    `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`
---
+++ main.py
@@ -0,0 +1,34 @@
+from google.cloud import bigquery
+import pandas as pd
+import base64
+from IPython.display import HTML
+from IPython.display import FileLinks
+import datetime
+
+
+
+date = datetime.datetime(2016, 8, 1)
+
+
+# Initiate bigquery client
+bq = bigquery.Client()
+
+
+for i in range(3):
+    # Get the first table
+    dt = date.strftime("%Y%m%d")
+    print(dt)
+
+    query = f"""
+    SELECT
+        *
+    FROM
+        `bigquery-public-data.google_analytics_sample.ga_sessions_{dt}`
+    """
+
+    # Create the query and convert to dataframe
+    result = bq.query(query).to_dataframe()
+
+    # View result
+    result.to_csv(f'C:\\dev\\bigquerydata\\ga_sessions_{dt}.csv')
+    date += datetime.timedelta(days=1)
